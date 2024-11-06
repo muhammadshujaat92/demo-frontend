6ee3c37422ea52aefacd117c225d0f09cccecb1a
muhammadshujaat92 <muhammadshujaat04@gmail.com>
2024-11-06 21:16:22 +0500

    banner image fixing test 3

--- src/app/_(Component)/BlogContent.jsx
+++ src/app/_(Component)/BlogContent.jsx
@@ -48,7 +48,7 @@ const BlogContent = ({ blogContent }) => {
                                 priority
                                 fetchPriority="high"
                                 placeholder="blur"
-                                blurDataURL="/imgs/homeSection1BlurData.jpg"
+                                blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEASABIAAD/2wBDAAoHBwgHBgoICAgLCgoLDhgQDg0NDh0VFhEYIx8lJCIfIiEmKzcvJik0KSEiMEExNDk7Pj4+JS5ESUM8SDc9Pjv/2wBDAQoLCw4NDhwQEBw7KCIoOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozv/wAARCAAGAAoDASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAAAAQF/8QAHxAAAQQCAgMAAAAAAAAAAAAAAgABAxEEBSEiEoGh/8QAFQEBAQAAAAAAAAAAAAAAAAAABQb/xAAaEQEAAgMBAAAAAAAAAAAAAAABACECAxOh/9oADAMBAAIRAxEAPwC/SQz4OrACyjkkJuxFy116f6sw8Dc+ZVso6vjo7IiWcAKk532Lb4T/2Q=="
                             />
                         ) : (
                             <Image
--- src/app/_(Component)/TourPage.jsx
+++ src/app/_(Component)/TourPage.jsx
@@ -36,7 +36,7 @@ const TourPage = ({ pageData }) => {
                                 layout="fill"
                                 fetchPriority="high"
                                 placeholder="blur"
-                                blurDataURL="/imgs/homeSection1BlurData.jpg"
+                                blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEASABIAAD/2wBDAAoHBwgHBgoICAgLCgoLDhgQDg0NDh0VFhEYIx8lJCIfIiEmKzcvJik0KSEiMEExNDk7Pj4+JS5ESUM8SDc9Pjv/2wBDAQoLCw4NDhwQEBw7KCIoOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozv/wAARCAAGAAoDASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAAAAQF/8QAHxAAAQQCAgMAAAAAAAAAAAAAAgABAxEEBSEiEoGh/8QAFQEBAQAAAAAAAAAAAAAAAAAABQb/xAAaEQEAAgMBAAAAAAAAAAAAAAABACECAxOh/9oADAMBAAIRAxEAPwC/SQz4OrACyjkkJuxFy116f6sw8Dc+ZVso6vjo7IiWcAKk532Lb4T/2Q=="
                                 priority
                             />
                         ) : (
@@ -50,7 +50,7 @@ const TourPage = ({ pageData }) => {
                         )
                     }
                     <div className="flex justify-center inset-0 absolute bg-black bg-opacity-50">
-                        <div className='absolute xl:top-32 w-full max-w-[1250px] flex flex-col gap-8 ps-3'>
+                        <div className='absolute md:top-32 w-full max-w-[1250px] flex flex-col gap-8 ps-3'>
                             <h1 className='text-[42px] font-sancoaleSoftened text-white'>{bannerHeading}</h1>
                             <p className='lg:text-lg text-white lg:font-bold'>{bannerParagraph}</p>
                         </div>
