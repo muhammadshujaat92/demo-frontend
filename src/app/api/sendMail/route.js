98e8da670c49f7e5a147de191e70eaf4ec499e0f
muhammadshujaat92 <muhammadshujaat04@gmail.com>
2024-11-14 16:06:13 +0500

    email sending test 3

--- src/app/api/sendMail/route.js
+++ src/app/api/sendMail/route.js
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     host: 'mail.indiayaatra.com',
-    port: 25,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
